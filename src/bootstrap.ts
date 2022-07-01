3a758968c14e42492ace006a2f7a2eabd508a893
walid <walid.karray@gmail.com>
2022-07-01 21:38:07 +0200

    refactor

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -2,11 +2,16 @@ import "reflect-metadata";
 import { ContainerBuilder } from "diod";
 import { DataSetClient } from "./clients/dataset.client";
 import { PlayerService } from "./services/player.service";
+
 console.debug("registering services in the Container ...");
 const builder = new ContainerBuilder();
+
+// services registration
 console.debug("container.register ▶️", DataSetClient.name);
 builder.registerAndUse(DataSetClient);
 console.debug("container.register ▶️️", PlayerService.name);
 builder.registerAndUse(PlayerService);
+
+// build container
 export const container = builder.build();
 console.debug("container is ready");
